fba8baada49b108bd00789c01f1a0ab58aa648b5
Lane Flint <lane.blair.flint@gmail.com>
2025-03-03 21:52:15 -0600

    Added abstract class middle earth character along with fields and methods. Fields contain name, health, and power. Methods contain a constructor, attack, get race, and display info

--- src/edu/mu/middleearth/characters/MiddleEarthCharacter.java
+++ src/edu/mu/middleearth/characters/MiddleEarthCharacter.java
@@ -5,6 +5,19 @@ public abstract class MiddleEarthCharacter {
 	private Double health;
 	private Double power;
 	
+	public MiddleEarthCharacter(String name, Double health, Double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	public abstract boolean attack(MiddleEarthCharacter target);
+	
+	public abstract String getRace();
+	
+	public void displayInfo() {
+		System.out.println(name + "'s" + "stats are: health = " + health + ", power = " + power);
+	}
 	
 
 }
