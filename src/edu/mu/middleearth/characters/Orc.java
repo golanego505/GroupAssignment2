eeda57775ba688bfdb30a7d66d81a8e0fd46f13d
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-03-03 23:37:03 -0600

    Creating Orc subclass. Implemented abstract methods attack, which attacks certain races with specific logic, and getRace.

---
+++ src/edu/mu/middleearth/characters/Orc.java
@@ -0,0 +1,41 @@
+package edu.mu.middleearth.characters;
+
+public class Orc extends MiddleEarthCharacter{
+
+	public Orc(String name, Double health, Double power) {
+		super(name, health, power);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean attack(MiddleEarthCharacter target) {
+		
+		double criticalStrike = this.getPower()*1.5;
+		
+		if(target instanceof Elf) {
+			System.out.println("Attack ineffective against: " + target.getRace());
+			return false;
+		}
+		else if(target instanceof Orc) {
+			System.out.println("Attack was against their own kin: " + target.getRace());
+			return false;
+		}
+		else if(target instanceof Dwarf || target instanceof Wizard) {
+			// Do normal attack damage
+			target.setHealth(target.getHealth() - this.getPower());
+			System.out.println(this.getName() + " smashes " + target.getName() + " with a Rebar club for " + this.getPower() + ".");
+		} else {
+			// Super effective attack 1.5 times damage against Humans
+			target.setHealth(target.getHealth() - criticalStrike);
+			System.out.println(this.getName() + " hits a critical " + target.getName() + " with a Rebar club for " + criticalStrike + ".");
+		}
+		
+		//target.setHealth(targetHealth);
+		return true;
+	}
+
+	@Override
+	public String getRace() {
+		return "Orc";
+	}	
+}
