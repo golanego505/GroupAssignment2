613d4eaffd5b475dbf3a39132a39a522008698bb
Lane Flint <lane.blair.flint@gmail.com>
2025-03-04 20:20:46 -0600

    Created add character method. Created display all characters method

--- src/edu/mu/middleearth/charactermanagementsystem/CharacterManager.java
+++ src/edu/mu/middleearth/charactermanagementsystem/CharacterManager.java
@@ -1,5 +1,46 @@
 package edu.mu.middleearth.charactermanagementsystem;
 
-public class CharacterManager {
+import edu.mu.middleearth.characters.MiddleEarthCharacter;
 
+public class CharacterManager {
+	private MiddleEarthCharacter[] characters;
+	
+	private int size = 1;
+	private int index = 0;
+	
+	
+	boolean addCharacter(MiddleEarthCharacter c) {
+		if(index == 0) {
+			characters = new MiddleEarthCharacter[size];
+			characters[index] = c;
+			index++;
+			size++;
+			return true;
+		}
+		MiddleEarthCharacter[] arrayX2;
+		if(size - 1 == index) {
+			size *= 2;
+			arrayX2 = new MiddleEarthCharacter[size];
+			
+			for(int i = 0; i < (index + 1); i++) {
+				arrayX2[i] = characters[i];
+			}
+			index++;
+			arrayX2[index] = c;
+			characters = arrayX2;
+			return true;
+		}
+		index++;
+		characters[index] = c;
+		return true;
+	}
+	
+	private void displayAllCharacters() {
+		if(characters != null) {
+			for(int i = 0; i < (index + 1); i++) {
+				characters[i].displayInfo();
+			}
+		}
+	}
+	
 }
