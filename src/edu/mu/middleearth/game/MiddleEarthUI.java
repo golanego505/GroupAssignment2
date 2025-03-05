c2bbd12db04501ae70d412ca40bc8889f6b917bc
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-03-05 00:20:12 -0600

    Created a getMenuInput method that gets and validates the users menu choice.

--- src/edu/mu/middleearth/game/MiddleEarthUI.java
+++ src/edu/mu/middleearth/game/MiddleEarthUI.java
@@ -1,16 +1,62 @@
 package edu.mu.middleearth.game;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import edu.mu.middleearth.charactermanagementsystem.CharacterManager;
+import edu.mu.middleearth.characters.MiddleEarthCharacter;
 
 public class MiddleEarthUI {
 	
-	// Method to gather user input 
-	public int getUserInput() {
-		return 0;
+	/**
+	 * Gets user input for a menu option.
+	 * Ensures the input is an integer between 1 and 6.
+	 * 
+	 * @return the selected menu option (1-6)
+	 */
+	public int getMenuInput() {
+		
+		boolean validInput = false;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(!validInput) {
+			try {
+				System.out.print("Choose Menu Option (1-6): ");
+				int menuChoice = scanner.nextInt();
+				scanner.nextLine();
+				
+				// Choice must be between 1-6
+				if(menuChoice < 1 || menuChoice > 6) {
+					System.out.println("Invalid input. Please enter an int 1-6.");
+					continue;
+				}
+				
+				validInput = true;
+				scanner.close();
+				return menuChoice;
+			} 
+			catch (InputMismatchException e){
+				System.out.println("Invalid input. Please enter an int 1-6.");
+				scanner.nextLine();
+			}
+		}
+		return 6; // Exit code if something goes wrong
 	}
 	
-	public void addCharacter(CharacterManager manager) {
-		
+
+	public MiddleEarthCharacter getCharacter() {
+		return null;
+		//prompt user to create a character
+		//add using addCharacter from manager
+	}
+	
+	// Displays the Menu used to play the game
+	public void displayMenu() {
+		System.out.println("1. Add a new character.");
+		System.out.println("2. View all characters.");
+		System.out.println("3. Update a character.");
+		System.out.println("4. Delete a character.");
+		System.out.println("5. Execute all characters' attack actions.");
+		System.out.println("6. Exit.");
 	}
 	
 	
