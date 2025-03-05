57c93b7aa26f2f833e6a2324f13a6f738bebf7df
Lane Flint <lane.blair.flint@gmail.com>
2025-03-04 19:12:14 -0600

    Created singleton class methods and functionality.

--- src/edu/mu/middleearth/charactermanagementsystem/MiddleEarthCouncil.java
+++ src/edu/mu/middleearth/charactermanagementsystem/MiddleEarthCouncil.java
@@ -1,5 +1,23 @@
 package edu.mu.middleearth.charactermanagementsystem;
 
 public class MiddleEarthCouncil {
-
+	
+	private static MiddleEarthCouncil instance = null;
+	
+	private CharacterManager manager;
+	
+	private MiddleEarthCouncil() {
+		manager = new CharacterManager();
+	}
+	
+	public static MiddleEarthCouncil getInstance() {
+		if (instance == null) {
+			instance = new MiddleEarthCouncil();
+		}
+		return instance;
+	}
+	
+	public CharacterManager getCharacterManager() {
+		return manager;
+	}
 }
