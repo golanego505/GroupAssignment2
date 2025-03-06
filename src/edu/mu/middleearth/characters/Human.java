9b8d3327823cfbaa5e9be4fdf7aab5a5c9a84315
Burnablelemming <114361388+Burnablelemming@users.noreply.github.com>
2025-03-05 22:47:15 -0600

    Fixed comments to have proper Javadoc format.

--- src/edu/mu/middleearth/characters/Human.java
+++ src/edu/mu/middleearth/characters/Human.java
@@ -2,16 +2,20 @@ package edu.mu.middleearth.characters;
 
 public class Human extends MiddleEarthCharacter{
 
-	/*
+	/**
 	 * Constructor method
 	 * Initializes all fields
 	 * Fields are name, health, and power
+	 *
+	 * @param name Human name 
+	 * @param health Human health
+	 * @param power Human attack damage
 	 */
 	public Human(String name, Double health, Double power) {
 		super(name, health, power);
 	}
 
-	/*
+	/**
 	 * Attack function takes a MiddleEarthCharacter and subtracts from the health depending on how strong the sustained attack was
 	 * Returns a boolean, true if attack was successful, false if attack did no damage.
 	 * Certain MiddleEarthCharacters will have races in which they have normal effectiveness, increased effectiveness, or no effect whatsoever
@@ -51,7 +55,11 @@ public class Human extends MiddleEarthCharacter{
 		return true;
 	}
 
-	
+	/**
+	 * Get character race
+	 * 
+	 * @return Human race as a string
+	 */
 	@Override
 	public String getRace() {
 		return "Human";
